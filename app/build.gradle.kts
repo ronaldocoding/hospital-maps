6781cdcae3d84c82df672b167ab3cf43ecfdcef9
ronaldocosta <ronaldocosta.developer@gmail.com>
2025-04-22 22:32:38 -0400

    add Mapbox Compose dependency to build configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.mapbox.maps.android)
+    implementation(libs.mapbox.maps.compose)
 
     testImplementation(libs.junit)
 
