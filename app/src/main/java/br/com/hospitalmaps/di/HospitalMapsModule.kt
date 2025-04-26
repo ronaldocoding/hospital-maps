afad998f108a96b88633b69250ad8e2a189b93aa
ronaldocosta <ronaldocosta.developer@gmail.com>
2025-04-26 11:43:46 -0400

    add Places API initialization to hospitalMapsModule for location services

--- app/src/main/java/br/com/hospitalmaps/di/HospitalMapsModule.kt
+++ app/src/main/java/br/com/hospitalmaps/di/HospitalMapsModule.kt
@@ -1,11 +1,17 @@
 package br.com.hospitalmaps.di
 
+import br.com.hospitalmaps.BuildConfig
 import br.com.hospitalmaps.data.repository.LocationRepository
 import com.google.android.gms.location.LocationServices
+import com.google.android.libraries.places.api.Places
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val hospitalMapsModule = module {
     single { LocationServices.getFusedLocationProviderClient(androidContext()) }
+    single { Places.initializeWithNewPlacesApiEnabled(androidContext(), providePlacesApiKey()) }
+    single { Places.createClient(androidContext()) }
     factory { LocationRepository(locationProviderClient = get(), context = androidContext()) }
 }
+
+private fun providePlacesApiKey() = BuildConfig.PLACES_API_KEY
