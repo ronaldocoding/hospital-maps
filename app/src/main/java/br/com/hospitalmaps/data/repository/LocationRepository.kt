3e75cd49ff382837eb81501e245289d8ffa7833c
ronaldocosta <ronaldocosta.developer@gmail.com>
2025-04-25 22:00:50 -0400

    add LocationRepository to hospitalMapsModule for location management

---
+++ app/src/main/java/br/com/hospitalmaps/data/repository/LocationRepository.kt
@@ -0,0 +1,44 @@
+package br.com.hospitalmaps.data.repository
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.flowOn
+
+@SuppressLint("MissingPermission")
+class LocationRepository(
+    private val locationProviderClient: FusedLocationProviderClient,
+    private val context: Context
+) {
+    fun getLastLocation(): Flow<Location> = callbackFlow {
+        if (hasNotLocationPermissions()) throw SecurityException(
+            "The permissions ACCESS_COARSE_LOCATION and ACCESS_FINE_LOCATION are required"
+        )
+        locationProviderClient.lastLocation
+            .addOnSuccessListener { location ->
+                runCatching { trySend(requireNotNull(location)) }
+                    .onFailure { close(it) }
+            }.addOnFailureListener {
+                close(it)
+            }
+
+        awaitClose()
+    }.flowOn(Dispatchers.IO)
+
+    private fun hasNotLocationPermissions() =
+        ActivityCompat.checkSelfPermission(
+            context,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+            context,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ) != PackageManager.PERMISSION_GRANTED
+}
--- app/src/main/java/br/com/hospitalmaps/di/HospitalMapsModule.kt
+++ app/src/main/java/br/com/hospitalmaps/di/HospitalMapsModule.kt
@@ -1,9 +1,11 @@
 package br.com.hospitalmaps.di
 
+import br.com.hospitalmaps.data.repository.LocationRepository
 import com.google.android.gms.location.LocationServices
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val hospitalMapsModule = module {
     single { LocationServices.getFusedLocationProviderClient(androidContext()) }
+    factory { LocationRepository(locationProviderClient = get(), context = androidContext()) }
 }
