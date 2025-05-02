827ee3c984c88f149a32bba48f7e709f4198a800
ronaldocosta <ronaldocosta.developer@gmail.com>
2025-05-02 16:02:43 -0400

    feat: add additional parameter to HospitalData for nearest hospital in HomeUiState

--- app/src/main/java/br/com/hospitalmaps/presentation/home/state/HomeUiState.kt
+++ app/src/main/java/br/com/hospitalmaps/presentation/home/state/HomeUiState.kt
@@ -4,7 +4,7 @@ import br.com.hospitalmaps.domain.model.HospitalData
 import br.com.hospitalmaps.domain.model.UserLocation
 
 data class HomeUiState(
-    val nearestHospital: HospitalData = HospitalData("", 0.0, 0.0),
+    val nearestHospital: HospitalData = HospitalData("", 0.0, 0.0,  0f),
     val userLocation: UserLocation = UserLocation(0.0, 0.0),
     val isLoading: Boolean = true
 )
