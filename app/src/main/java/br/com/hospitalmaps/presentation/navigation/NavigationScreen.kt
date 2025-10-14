23f675621a0ff3ce09e72571d1042d5e5eaf97b1
Ronaldo Costa <ronaldo.costa@ifood.com.br>
2025-10-14 19:35:59 -0400

    refactor: remove unused imports in NavigationScreen

--- app/src/main/java/br/com/hospitalmaps/presentation/navigation/NavigationScreen.kt
+++ app/src/main/java/br/com/hospitalmaps/presentation/navigation/NavigationScreen.kt
@@ -4,8 +4,6 @@ import android.annotation.SuppressLint
 import android.util.Log
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
