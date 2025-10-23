01913c65b0c06144744be704aa3219a9e75a59de
Ronaldo Costa <ronaldo.costa@ifood.com.br>
2025-10-22 20:37:33 -0400

    Add button to navigate to the nearest hospital

--- app/src/main/java/br/com/hospitalmaps/presentation/home/view/HomeScreen.kt
+++ app/src/main/java/br/com/hospitalmaps/presentation/home/view/HomeScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -29,7 +30,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -46,7 +46,6 @@ import br.com.hospitalmaps.shared.statusBarHeightDp
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MapStyleOptions
-import com.google.android.material.color.MaterialColors
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.MapUiSettings
@@ -151,6 +150,7 @@ private fun HomeContent(
     }
     if (nearbyHospitals.isEmpty().not() && userLocation.isEmpty().not()) {
         Scaffold(
+            contentWindowInsets = WindowInsets(0, 0, 0, 0),
             floatingActionButton = {
                 IconButton(
                     modifier = Modifier
@@ -160,7 +160,7 @@ private fun HomeContent(
                             shape = RoundedCornerShape(12.dp)
                         ),
                     onClick = {
-                        // TODO
+                        onNavigate(hospitalPoints[0])
                     }
                 ) {
                     Icon(
